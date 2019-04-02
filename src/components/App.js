484156bbf2b2d676bd5739df381739fe718225ad
Denis <disamapm@gmail.com>
2019-04-02 17:27:08 +0300

    Used 'create-react-app' for fast creating application

---
+++ src/components/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <header>
+          <a href="https://reactjs.org/" target="_blank">
+            <img
+              src="./img/logo.svg"
+              width="64"
+              height="64"
+              className="App-logo"
+            />
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
